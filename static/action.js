461f2645a9ecaf1e87c10c96a6b358d94c12036b
Abdurrahman BULUT <abdurrahmanbulut@yahoo.com>
2022-01-13 21:14:04 +0300

    user adding, removing, editing and image adding, editing are added.

--- static/action.js
+++ static/action.js
@@ -1,30 +1,47 @@
+
 $(document).ready(function(){
+
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myTable tbody tr").filter(function() {
+            if( typeof value != "string"){
+                value = value.toString();
+            }
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+
+
     $('[data-toggle="tooltip"]').tooltip();
     var actions = $("table td:last-child").html();
     // Append table with add row form on add new button click
     $(".add-new").click(function(){
         $(this).attr("disabled", "disabled");
-        var index = $("table tbody tr:last-child").index();
+        var index = $("table tbody tr:first-child").index();
         var row = '<tr>' +
-            '<td><input type="text" class="form-control" name="image" id="txtimage"></td>' +
+            '<td><img src="../static/image/anonymous.png" alt="Costumer image"></td> ' +
             '<td><input type="text" class="form-control" name="name" id="txtname"></td>' +
             '<td><input type="text" class="form-control" name="email" id="txtemail"></td>' +
             '<td><input type="text" class="form-control" name="phone" id="txtphone"></td>' +
-            '<td><input type="text" class="form-control" name="password" id="txtpassword"></td>' +
+            '<td class="hidetext"><input type="text" class="form-control" name="password" id="txtpassword"></td>' +
             '<td><input type="text" class="form-control" name="type" id="txttype"></td>' +
+            '<td></td>' +
             '<td>' + actions + '</td>' +
         '</tr>';
-        $("table").append(row); 
+        $("table").prepend(row); 
         
-        $("table tbody tr").eq(index + 1).find(".add, .edit, .delete").toggle();
+        $("table tbody tr").eq(index).find(".add, .edit, .delete").toggle();
         $('[data-toggle="tooltip"]').tooltip();
  
     });
    
+
+
     // Add row on add button click
     $(document).on("click", ".add", function(){
         var empty = false;
         var input = $(this).parents("tr").find('input[type="text"]');
+
         input.each(function(){
             if(!$(this).val()){
                 $(this).addClass("error");
@@ -33,16 +50,20 @@ $(document).ready(function(){
                 $(this).removeClass("error");
             }
         });
-        var txtimage = $("#txtimage").val();
+
         var txtname = $("#txtname").val();
         var txtemail = $("#txtemail").val();
         var txtphone = $("#txtphone").val();
         var txtpassword = $("#txtpassword").val();
         var txttype = $("#txttype").val();
-        $.post("/ajax_add", { txtimage: txtimage, txtname: txtname, txtemail: txtemail, txtphone: txtphone, txtpassword: txtpassword, txttype: txttype}, function(data) {
-            $("#displaymessage").html(data);
+        $.post("/ajax_add", {txtname: txtname, txtemail: txtemail, txtphone: txtphone, txtpassword: txtpassword, txttype: txttype}, function(data) {
+            
+            var message = data.split(",");
+            $("table tbody tr:first-child").attr('id', message[1]);
+            $("#displaymessage").html(message[0]);
             $("#displaymessage").show();
         });
+
         $(this).parents("tr").find(".error").first().focus();
         if(!empty){
             input.each(function(){
@@ -51,23 +72,26 @@ $(document).ready(function(){
             $(this).parents("tr").find(".add, .edit, .delete").toggle();
             $(".add-new").removeAttr("disabled");
         } 
-    });
+    }); 
     // Delete row on delete button click
     $(document).on("click", ".delete", function(){
+       
+
+        var currentRow=$(this).parents("tr").attr("id");
         $(this).parents("tr").remove();
-        $(".add-new").removeAttr("disabled");
-        var id = $(this).attr("id");
-        var string = id;
-        $.post("/ajax_delete", { string: string}, function(data) {
+
+        $.post("/ajax_delete", {userKey: currentRow}, function(data) {
             $("#displaymessage").html(data);
             $("#displaymessage").show();
         });
+        
     });
     // update rec row on edit button click
     $(document).on("click", ".update", function(){
 
         var empty = false;
         var input = $(this).parents("tr").find('input[type="text"]');
+
         input.each(function(){
             if(!$(this).val()){
                 $(this).addClass("error");
@@ -78,32 +102,34 @@ $(document).ready(function(){
         });
 
         var currentRow=$(this).closest("tr"); 
-      
-        var txtimage = $('#txtimage').val();
+        
         var txtname = $('#txtname').val();
         var txtemail = $('#txtemail').val();
         var txtphone = $('#txtphone').val();
         var txtpassword = $('#txtpassword').val();
-        var txttype =$('#txttype').val();
-        var txtuid = $('#txtuid').val();
-        var txtkey= $('#txtkey').val();
+        var txttype = $('#txttype').val();
+        var currentRow=$(this).parents("tr").attr("id");
 
-        $.post("/ajax_update", {  txtimage: txtimage, txtname: txtname, txtemail: txtemail, txtphone: txtphone, txtpassword: txtpassword, txttype: txttype, txtuid: txtuid, txtkey: txtkey}, function(data) {
+      
+
+        $.post("/ajax_update", {txtname: txtname, txtemail: txtemail, txtphone: txtphone, txtpassword: txtpassword, txttype: txttype, txtkey: currentRow}, function(data) {
             $("#displaymessage").html(data);
             $("#displaymessage").show();
         });
         $(this).parents("tr").find(".update").removeClass("update").addClass("add");
         $(this).parents("tr").find(".error").first().focus();
+
         if(!empty){
             input.each(function(){
                 $(this).parent("td").html($(this).val());
             });   
+          
             $(this).parents("tr").find(".add, .edit").toggle();
             $(".add-new").removeAttr("disabled");
         }
-         
-         
+      
     });
+ 
     // Edit row on edit button click
     $(document).on("click", ".edit", function(){  
         $(this).parents("tr").find("td:not(:last-child)").each(function(i){
@@ -120,17 +146,56 @@ $(document).ready(function(){
             }else if (i=='5'){
                 var idname = 'txttype';
             }else if (i=='6'){
-                var idname = 'txtuid';
+                var idname = 'txtwallet';
             }else if (i=='7'){
                 var idname = 'txtkey';
             }
-            else{} 
-            $(this).html('<input type="text" name="updaterec" id="' + idname + '" class="form-control" value="' + $(this).text() + '">');
+            else{}
+
+            if(i == '0' || i == '6' || i == '7'){
+
+
+            }else if(i == '5'){
+                $(this).html('<input type="text" name="updaterec" id="' + idname + '" class="form-control" value="' + $(this).text() + '">');
+            }
+            else{
+             $(this).html('<input type="text" name="updaterec" id="' + idname + '" class="form-control" value="' + $(this).text() + '">');
+            }
+
         });  
         $(this).parents("tr").find(".add, .edit").toggle();
         $(".add-new").attr("disabled", "disabled");
         $(this).parents("tr").find(".add").removeClass("add").addClass("update"); 
     });
 
+    $(document).on("click", ".imageUpload", function(){
+       
+        $(this).css( "display", "none" );
+        $(this).siblings().css( "display", "" );
+        $(this).siblings().children("div").children("div").children("input").attr('id', 'files');
+        $(this).siblings().children("div").children("div").children("label").attr('for', 'files');
+        $(this).siblings().children("div").children("div").children("div").children(".cancelUpload").siblings().attr('id', 'send');
+        $(this).siblings().children("div").children("div").siblings().children("p").attr('id', 'uploading');
+        $(this).siblings().children("div").children("div").siblings().children("progress").attr('id', 'progress');
+
+    });
+    $(document).on("click", ".cancelUpload", function(){
+       
+        $(this).parent().parent().parent().parent(".imageInput").siblings().css( "display", "" );
+        $(this).parent().parent().parent().parent(".imageInput").css( "display", "none" );
+        $(this).parent().siblings("input").attr('id', 'filetemp');
+        $(this).parent().siblings("label").attr('for', 'filetemp');
+        $(this).siblings().attr('id', '');
+        $(this).parent().parent().siblings().children("p").attr('id', '');
+        $(this).parent().parent().siblings().children("progress").attr('id', '');
+
+
+    });
+  
+   
+
     
 });
+
+
+
